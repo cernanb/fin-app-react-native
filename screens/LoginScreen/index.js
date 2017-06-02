a1eb8f49ca921ffe136a9b992988013cfa85c696
Cernan Bernardo <cernanb@gmail.com>
2017-06-02 09:32:43 -0700

    Add redux to LoginScreen component

--- screens/LoginScreen/index.js
+++ screens/LoginScreen/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import {
-    View, 
-    Text,
-    StyleSheet 
+  Button,
+  View, 
+  Text,
+  StyleSheet
 } from 'react-native'
+import { connect } from 'react-redux'
 
 import LoginForm from './LoginForm'
+import { login } from '../../redux/modules/Auth/actions'
 
 class LoginScreen extends Component {
   static navigationOptions = {
@@ -14,12 +17,19 @@ class LoginScreen extends Component {
 
   render() {
     return(
-      <View style={styles.container}><LoginForm /></View>
+      <View style={styles.container}>
+        <Text>Login</Text>
+        <LoginForm />
+        <Button 
+          title="Create new account"
+          onPress={() => console.log('got to signup page')}
+        />
+      </View>
     )
   }
 }
 
-export default LoginScreen
+export default connect(null, { login })(LoginScreen)
 
 const styles = StyleSheet.create({
   container: {
