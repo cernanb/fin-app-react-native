140e3d8821e8b781156b8c6dab20009f747bc9a0
Cernan Bernardo <cernanb@gmail.com>
2017-06-02 09:17:37 -0700

    remove verdana font

--- components/Label/index.js
+++ components/Label/index.js
@@ -18,7 +18,6 @@ const Label = (props) => {
 const styles = StyleSheet.create({
     textLabel: {
         fontSize: 20,
-        fontWeight: 'bold',
         fontFamily: 'Verdana',
         marginBottom: 10,
         color: '#595856'
