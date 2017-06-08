682cb5a2cd972d431ace7e5b401b9c2b98510b41
Cernan Bernardo <cernanb@gmail.com>
2017-06-08 11:55:10 -0700

    token now being set in asyncstorage and actions dispatched for token and reset login

--- redux/modules/Auth/actions.js
+++ redux/modules/Auth/actions.js
@@ -1,3 +1,30 @@
-export const login = (formData, navigation) => {
-    console.log(formData)
+import { AsyncStorage } from 'react-native'
+import { reset } from 'redux-form'
+
+const TOKEN_KEY = 'fin-token';
+
+const setCurrentUser = formData => {
+    return {
+        type: 'SET_CURRENT_USER',
+        currentUser: formData
+    }
 }
+
+const updateStorageToken = async (dispatch, formData, navigate) => {
+    try {
+        dispatch({ type: 'SETTING_ASYNC_STORAGE_ITEM' })
+        await AsyncStorage.setItem('tokenKey', TOKEN_KEY)
+        dispatch(setCurrentUser(formData))
+        dispatch(reset('login'))
+        navigate('Home')
+    } catch(error) {
+        console.log('You failed misserably with AsyncStorage')
+        dispatch({ type: 'FAILED_ASYNC_STORAGE' })
+    }
+}
+
+export const login = (formData, navigate) => {
+    console.log('hi')
+    return dispatch => updateStorageToken(dispatch, formData, navigate)
+}
+
