c4fa669431899c9df6f1302cf228aaa1c8aebc11
Cernan Bernardo <cernanb@gmail.com>
2017-06-15 22:10:24 -0700

    logout functionality working with alerts

--- screens/UserProfileScreen/index.js
+++ screens/UserProfileScreen/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import {
+    AlertIOS,
+    AsyncStorage,
+    Button,
     Text,
     View,
     StyleSheet
@@ -7,14 +10,50 @@ import {
 
 import { connect } from 'react-redux'
 
+// TODO add TabNavigator with Profile buttons 
+
 class Profile extends Component {
     render() {
         return (
             <View>
-                <Text>{this.props.user.username}</Text>
+                <Text>{this.props.user.username}'s Profile</Text>
             </View>
         )
     }
 }
+
+Profile.navigationOptions = ({ navigation }) => ({
+    headerTitle: 'Profile',
+    headerLeft: (
+        <Button //Todo abstract this into components to use in other screens
+            title="Logout" 
+            onPress={() => {
+                AlertIOS.alert(
+                    'Are you sure?',
+                    null,
+                    [
+                        { 
+                            text: 'Cancel', 
+                            onPress: () => console.log('logout cancelled'),
+                            style: 'cancel'
+                        }, 
+                        { 
+                            text: 'Yes', 
+                            onPress: async() => {
+                                try {
+                                    await AsyncStorage.removeItem('tokenKey')
+                                    console.log(navigation.navigate('Login'))
+                                } catch(error) {
+                                    // Todo add notification that this failed
+                                    console.log(error)
+                                }
+                            }
+                        }
+                    ]
+                )
+            }} 
+        />
+    )
+})
 
 export default connect(state => ({user: state.auth}), null)(Profile)
