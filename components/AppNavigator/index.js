661d5da4f73cd7f8f12992407e3da843a21d77c4
Cernan Bernardo <cernanb@gmail.com>
2017-06-17 22:51:56 -0700

    add ProfileTabNav to AppNavigator

--- components/AppNavigator/index.js
+++ components/AppNavigator/index.js
@@ -3,13 +3,13 @@ import { StackNavigator, addNavigationHelpers } from 'react-navigation'
 // Grab a list of screens 
 import HomeScreen from '../../screens/HomeScreen'
 import LoginScreen from '../../screens/LoginScreen'
-import UserProfileScreen from '../../screens/UserProfileScreen'
+import UserProfileTabNavScreen from '../../screens/UserProfileTabNavScreen'
 import SignupScreen from '../../screens/SignupScreen'
 
 // Add Screen to stack
 export default AppNavigator = StackNavigator({
   Home: { screen: HomeScreen },
   Login: { screen: LoginScreen },
-  UserProfile: { screen: UserProfileScreen },
+  ProfileTabNav: { screen: UserProfileTabNavScreen },
   Signup: { screen: SignupScreen },
 })
