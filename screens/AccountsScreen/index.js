c133e5d0477abac5d95c2c2dab406067c950dbe4
Cernan Bernardo <cernanb@gmail.com>
2017-06-17 22:53:46 -0700

    start AccountsScreen

---
+++ screens/AccountsScreen/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import {
+    Text, 
+    View, 
+} from 'react-native'
+
+class AccountsScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    My Accounts
+                </Text>
+            </View>
+        )
+    }
+}
+
+export default AccountsScreen
