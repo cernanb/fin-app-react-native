a425a8f86a9d9a32c3322898f06e0e2550ace5d7
Cernan Bernardo <cernanb@gmail.com>
2017-06-17 22:54:06 -0700

    start LiabilitiesScreen component

---
+++ screens/LiabilitiesScreen/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import {
+    Text, 
+    View, 
+} from 'react-native'
+
+class LiabilitiesScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    My Liabilities
+                </Text>
+            </View>
+        )
+    }
+}
+
+export default LiabilitiesScreen
